5801cc293e84102ad21cb5a63433d563b6489ff1
Henny2 <henrieke.baunack@web.de>
2024-08-10 17:36:49 -1000

    Initial Commit

---
+++ StreetMarkets/StreetMarketsApp.swift
@@ -0,0 +1,17 @@
+//
+//  StreetMarketsApp.swift
+//  StreetMarkets
+//
+//  Created by Henrieke Baunack on 8/10/24.
+//
+
+import SwiftUI
+
+@main
+struct StreetMarketsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
